9f40bac8a0b72e84c036c989130a5c1486d207af
Lior Chamla <lchamla@gmail.com>
2020-01-29 16:58:26 +0100

    Ajout de la configuration YAML

--- config/configuration.php
+++ config/configuration.php
@@ -9,6 +9,8 @@
  * attend.
  */
 
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Route;
@@ -16,41 +18,39 @@ use Symfony\Component\Routing\RouteCollection;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// Création de la collection de routes disponibles pour notre application
-$routesCollection = new RouteCollection();
-
 /**
- * DECOUVERTE DES PARAMETRES SUPPLEMENTAIRES :
- * ---------------
- * Dans la section précédente, on a vu qu'on pouvait définir des valeurs par défaut pour des paramètres de route. MAIS ON PEUT FAIRE PLUS 
- * ENCORE ! On peut définir des valeurs par défaut SUPPLEMENTAIRES, on n'est pas limité aux paramètres de la route.
+ * DECOUVERTE DE LA CONFIGURATION YAML :
+ * -------------
+ * Le composant symfony/routing peut lire et interprêter un fichier YAML afin de construire la liste des routes ! Pour ça, il est nécessaire
+ * d'installer le composant symfony/yaml (composer require symfony/yaml) qui sert à lire ces fichiers de configuration !
  * 
- * Ici par exemple, j'ajoute un paramètre que j'appelle '_controller' et qui contient une chaine de caractères représentant la classe que je
- * veux instancier et la méthode que je veux appeler dessus !
+ * Par ailleurs, on aura aussi besoin du composant symfony/config (composer require symfony/config) qui nous aide notamment à trouver,
+ * lire, charger et valider des fichiers de configuration (pas dégueu hein ;))
  * 
- * Désormais, en tapant /hello/Lior, l'urlMatcher va me renvoyer le tableau suivant :
- * ['_route' => 'hello', 'name' => 'Lior', '_controller' => 'App\Controller\HelloController@sayHello']
+ * Allez voir le fichier configuration/routes.yml si ce n'est pas déjà fait, vous verrez qu'il reprend, dans un autre format bien sur,
+ * exactement les mêmes notions que ce qu'on a fait en PHP jusqu'à maintenant
  * 
- * Charge à moi ensuite de travailler sur la chaine _controller pour instancier la bonne classe et appeler la méthode sayHello() dessus !
- * Magique.
- * 
- * Bien sur, on fait ça pour toutes nos routes :)
- * 
- * IMPORTANT ET A NOTER :
- * -----------
- * J'ai CHOISI d'appeler cette information '_controller' parce que c'est ce qu'on voit le plus souvent (et que vous retrouvez dans le framework
- * Symfony) mais j'aurai pu choisir de l'appeler 'cocoLAsticot', C'EST ARBITRAIRE !
  */
-$routesCollection->add('hello', new Route('/hello/{name}', [
-    'name' => 'World',
-    '_controller' => 'App\Controller\HelloController@sayHello'
-]));
 
-$routesCollection->add('list', new Route('/', ['_controller' => 'App\Controller\TaskController@index']));
-$routesCollection->add('show', new Route('/show/{id}', ['_controller' => 'App\Controller\TaskController@show'], [
-    'id' => '\d+'
-]));
-$routesCollection->add('create', new Route('/create', ['_controller' => 'App\Controller\TaskController@create']));
+// Création de la collection de routes disponibles pour notre application
+// 1) On créé un localisateur de fichiers de configuration qui va chercher dans CE DOSSIER (on est dans le dossier config ;))
+$fileLocator = new FileLocator([__DIR__]);
+// 2) On créé un chargeur de fichier YAML spécifique aux routes, il se sert du locator pour trouver les fichiers yaml
+$routesYamlLoader = new YamlFileLoader($fileLocator);
+// 3) On charge la collection de routes du fichier routes.yml
+$routesCollection = $routesYamlLoader->load('routes.yml');
+
+// Remplace l'ancien code :
+// $routesCollection = new RouteCollection();
+// $routesCollection->add('hello', new Route('/hello/{name}', [
+//     'name' => 'World',
+//     '_controller' => 'App\Controller\HelloController@sayHello'
+// ]));
+// $routesCollection->add('list', new Route('/', ['_controller' => 'App\Controller\TaskController@index']));
+// $routesCollection->add('show', new Route('/show/{id}', ['_controller' => 'App\Controller\TaskController@show'], [
+//     'id' => '\d+'
+// ]));
+// $routesCollection->add('create', new Route('/create', ['_controller' => 'App\Controller\TaskController@create']));
 
 // 1) Construisons le RequestContext :
 $url = $_SERVER['PATH_INFO'] ?? '/'; // Si il n'y a rien dans le PATH_INFO c'est qu'on est sur "/"
--- index.php
+++ index.php
@@ -1,76 +1,23 @@
 <?php
 
 /**
- * QUATRIEME PARTIE : LES PARAMETRES SUPPLEMENTAIRES DANS UNE ROUTE (ET UN PEU DE POO) !
+ * CINQUIEME PARTIE : CONFIGURATION DES ROUTES AU FORMAT YAML
  * ----------------------
- * Dans la dernière section, nous avons découverts que nous pouvions travailler sur les paramètres des routes :
- * 1) Les requirements nous permettent de leur donner un cadre et des contraintes (exemple : /show/{id} avec un id numérique UNIQUEMENT)
- * 2) Les valeurs par défaut nous permettent de ne pas renseigner les paramètres obligatoirement dans l'URL (exemple : qu'on puisse appeler
- * /hello/Lior ou même /hello (sans donnée pour le paramètre {name}))
+ * Notre code fonctionne désormais très bien avec le composant symfony/routing, on a même réorganisé le tout avec des controllers.
  * 
- * On peut donc désormais comprendre une autre notion essentielle : ON PEUT ENRICHIR LES VALEURS PAR DEFAUT AVEC DES PARAMETRES PERSONNALISES.
+ * On créé la correspondance entre les controller et les routes grâce au paramètre supplémentaire "_controller" que l'on pose sur chaque
+ * route et qu'on décrypte ensuite dans index.php afin d'instancier le bon controller et d'appeler la bonne méthode en lui passant
+ * les paramètres de la route.
  * 
- * Prenons un exemple simple : imaginons la route /hello/{name}
- * - Elle ne contient qu'un seul paramètre : {name}
- * - On peut donc lui donner une valeur par défaut : ['name' => 'World'] qui veut dire que si on ne donne rien dans l'URL (/hello), alors name 
- * vaudra World. Si on appelle /hello, le matcher nous rendra le tableau suivant :
- * [
- *  '_route' => 'hello', // Nom de la route trouvée
- *  'name' => 'World' // Valeur du paramètre {name}
- * ]
- * - Mais on peut aussi donner des valeurs par défaut pour des paramètres qui ne sont pas VISIBLES : ['name' => 'World', 'autre' => 'toto']
- * Si on appelle le matcher avec /hello il nous rendra le tableau suivant :
- * [
- *  '_route' => 'hello',
- *  'name' => 'World',
- *  'autre' => 'toto' // Le matcher a inclus la valeur par défaut qu'on avait défini dans la route !
- * ]
- * 
- * BON, ET ALORS ? 
- * -------------
- * Sans en avoir l'air, c'est une fonctionnalité très puissante qui nous est proposée ici, puisqu'elle permet de donner les informations
- * supplémentaires que l'on souhaite pour chaque route ! Et si nous nous en servions pour expliquer à quelle action nous voulons associer
- * chaque route ? ;-)
- * 
- * INTRODUCTION DES CONTROLLERS :
- * --------------
- * Nous avons ajouté un dossier src/Controller qui contient deux nouvelles classes :
- * - HelloController (qui va gérer la route /hello/{name})
- * - TaskController (qui va gérer les routes /, /create et /show/{id})
- * 
- * Jusqu'à présent, on se servait simplement du nom des routes pour découvrir le fichier PHP à inclure. C'était simple et ça faisait bien
- * le boulot, mais ça demandait un fichier PHP par action de notre site. Pas pratique si notre site comporte des dizaines d'actions possibles.
- * 
- * J'ai donc créé deux classes qui concentrent en tout 4 actions (oui, donc 2 fichiers au lieu de 4 fichiers pour le même nombre d'actions)
- * Mais comment faire lorsqu'un URL est tapée pour savoir quelle classe doit s'en charger, et quelle méthode on doit appeler ?
- * 
- * FACILE : On ajoute à chaque route une valeur par défaut qui explique qui doit gérer la route ! Ensuite, le matcher nous rendra après son
- * analyse de l'URL tous les paramètres de la route DONT celui qui nous dit qui est la classe qui gère et quelle méthode on doit appeler !
- * 
- * UN CONSEIL :
- * ----------
- * Pour ne pas trop vous prendre la tête au départ, concentrez vous vraiment sur l'action /hello/{name} et le controller HelloController
- * Si vous comprenez cette partie simple, alors vous pourrez comprendre plus facilement ce qui se passe sur le controller TaskController
- * 
- * Les fichiers à vraiment bien examiner ici sont :
- * - config/configuration.php où on indique à chaque route quelle est la classe et la méthode qu'il faudra appeler pour la gérer
- * - index.php où on instancie la classe correspondante à une route et sur laquelle on appelle la méthode voulue en lui passant
- * les paramètres de la route pour qu'elle puisse les utiliser si elle veut.
+ * Ici, nous allons voir comment les composant symfony/yaml et symfony/config vont nous permettre de mettre en place toute la configuration
+ * des routes au sein d'un ou plusieurs fichiers YAML. Pour ça il faudra les installer (composer require symfony/yaml symfony/config)
  * 
  * -----------------
  * 
  * Pour suivre toutes les modifications apportées par rapport au code de base, regardez les fichiers suivants :
- * - composer.json : on ajoute une politique d'autoloading pour qu'il sache trouver où sont nos classes
- * - config/configuration.php : on ajoute pour chaque route un paramètre par défaut qui nous indique la classe à instancier et la méthode à 
- * appeler
- * - index.php : on met en place un algorithme pour instancier pour chaque route le bon controller et appeler la bonne méthode
- * - src/Controller/TaskController.php : une classe qui reprend les actions concernant les tâches (liste, détails et formulaire)
- * - src/Controller/HelloController.php : une classe qui reprend l'action hello (dire bonjour :D)
- * - pages/hello.php : on ne garde que le html
- * - pages/list.php : on ne garde que le html
- * - pages/create.php : on ne garde que le html
- * - pages/show.php : on ne garde que le html
- *
+ * - composer.json : on ajoute les dépendances à symfony/yaml et symfony/config
+ * - config/routes.yml : on créé un fichier de configuration yaml et on y reporte tout ce qu'on avait fait en PHP (la RouteCollection)
+ * - config/configuration.php : on remplace le code PHP qui créait la collection des routes par le code qui va charger le fichier YAML
  */
 
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
